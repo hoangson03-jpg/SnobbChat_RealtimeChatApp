9ca3d9ea856c8ab0307e64a2cf6b60a4a679cbe8
Son <billtommy0@gmail.com>
2026-05-17 21:47:58 +0700

    send OTP

--- backend/src/libs/sendEmailOTP.js
+++ backend/src/libs/sendEmailOTP.js
@@ -1,48 +1,24 @@
-import nodemailer from "nodemailer";
-
 export const sendOTP = async (email, otp) => {
-
     try {
-
         const transporter = nodemailer.createTransport({
-
-            host: "74.125.68.108", // Host cứng của google mail để tránh lỗi ipv6 không hỗ trợ
-
-            port: 587,
-
-            secure: false,
-
+            service: 'gmail',
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS
-            },
-
-            tls: {
-                servername: "smtp.gmail.com"
             }
         });
 
-        await transporter.verify();
-
-        console.log("SMTP READY");
-
+        // Không nhất thiết phải transporter.verify() mỗi lần gửi để giảm độ trễ
+        
         await transporter.sendMail({
-
             from: `"SnobbChat Support" <${process.env.EMAIL_USER}>`,
-
             to: email,
-
             subject: "OTP xác thực",
-
-            html: `<h1>${otp}</h1>`
-
+            html: `<h1>Mã OTP của bạn là: ${otp}</h1>`
         });
 
-        console.log("MAIL SENT");
-
+        console.log("MAIL SENT TO:", email);
     } catch (err) {
-
-        console.error(err);
-
+        console.error("LỖI GỬI MAIL:", err);
     }
 };
